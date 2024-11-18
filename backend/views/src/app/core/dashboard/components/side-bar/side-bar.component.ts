e624b3891ff85eea96f91c39b1adf72431857faa
Saleh <salehabuhussein2@gmail.com>
2024-11-18 20:15:04 +0300

    Add dashboard header with burger-icon sliding effect

--- backend/views/src/app/core/dashboard/components/side-bar/side-bar.component.ts
+++ backend/views/src/app/core/dashboard/components/side-bar/side-bar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+  constructor(public dashboardService: DashboardService) {
 
+  }
 }
--- backend/views/src/app/core/dashboard/dashboard.component.ts
+++ backend/views/src/app/core/dashboard/dashboard.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { DashboardService } from './services/dashboard.service';
+
+import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb.component";
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [
+    BreadcrumbComponent,
+    DashboardHeaderComponent,
+    NgClass,
+    RouterOutlet,
+    SideBarComponent,
+],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  constructor(public dashboardService: DashboardService) {
 
+  }
 }
