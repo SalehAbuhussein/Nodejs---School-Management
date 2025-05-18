fb0b4e2ddf14cfe8530359ca5d0fb138df59ba18
Saleh <salehabuhussein2@gmail.com>
2025-05-18 12:09:29 +0300

    login structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: MainLayoutComponent,
+    loadChildren: () => import('./core/auth/auth.routes').then(r => r.routes)
+  },
+];
