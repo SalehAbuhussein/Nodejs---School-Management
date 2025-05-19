6310a429d8c87cf7d7aca04a65a089d085b6ed9f
Saleh <salehabuhussein2@gmail.com>
2025-05-19 12:53:37 +0300

    Finish login form UI

--- frontend/src/app/core/auth/login/components/login-form/login-form.component.ts
+++ frontend/src/app/core/auth/login/components/login-form/login-form.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { BasicInputComponent } from 'app/shared/components/inputs/basic-input/basic-input.component';
+
+import { LoginForm } from './types/login-form.types';
+
 import { generalConstants } from 'constants/general-constants';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [
+    BasicInputComponent,
     RouterLink,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  loginForm: LoginForm = {
+    email: {
+      value: '',
+      error: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      error: '',
+      isValid: false
+    }
+  };
+
   constants = generalConstants;
 }
