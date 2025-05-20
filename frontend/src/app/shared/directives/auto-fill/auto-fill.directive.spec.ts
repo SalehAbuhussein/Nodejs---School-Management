7ab6fa1e98d602be0a642968bca568c5b3973ef7
Saleh <salehabuhussein2@gmail.com>
2025-05-20 11:25:17 +0300

    Create proxy for backend

--- frontend/src/app/shared/directives/auto-fill/auto-fill.directive.spec.ts
+++ frontend/src/app/shared/directives/auto-fill/auto-fill.directive.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef } from '@angular/core';
+
 import { AutoFillDirective } from './auto-fill.directive';
 
 describe('AutoFillDirective', () => {
   it('should create an instance', () => {
-    const directive = new AutoFillDirective();
+    const directive = new AutoFillDirective(new ElementRef(document.createElement('input')));
     expect(directive).toBeTruthy();
   });
 });
