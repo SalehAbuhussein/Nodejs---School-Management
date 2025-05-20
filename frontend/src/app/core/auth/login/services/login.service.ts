64f4ce1ce8cd773d0918ce5baf90ce41aa8c4ecb
Saleh <salehabuhussein2@gmail.com>
2025-05-20 13:46:58 +0300

    Add login service login template

--- frontend/src/app/core/auth/login/services/login.service.ts
+++ frontend/src/app/core/auth/login/services/login.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { ApiService } from 'app/core/services/api.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(public apiService: ApiService) { }
+
+  /**
+   * 
+   * @param username username form field 
+   * @param password password form field
+   */
+  login = (username: string, password: string) => {
+    this.apiService
+  }
 }
